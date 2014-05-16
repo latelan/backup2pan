0efb1bd6989fa58b2dcf9523279e151e232b3546
abel <coolboy1353@163.com>
2014-05-16 13:03:07 +0800

    add requirement.md

--- get_access_token.sh
+++ get_access_token.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-curl -k -L -d "grant_type=device_token&code=b7d502a870a4c63349a229e73072cee3&client_id=23znlt3cWsHgAsbmkIsU74Id&client_secret=BjhEl2xKDFD8ailMfD9hRRrOhabIUT0D" "https://openapi.baidu.com/oauth/2.0/token"
+curl -k -L -d "grant_type=device_token&code=04be557473ddaf1c9e449617271235dd&client_id=23znlt3cWsHgAsbmkIsU74Id&client_secret=BjhEl2xKDFD8ailMfD9hRRrOhabIUT0D" "https://openapi.baidu.com/oauth/2.0/token"
