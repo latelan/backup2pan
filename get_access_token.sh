ce4b02524767323931529d5c7fd02bbbaab5fb9c
abel <coolboy1353@163.com>
2014-05-16 13:06:31 +0800

    开发过程中的文件放在tools中

--- get_access_token.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-
-curl -k -L -d "grant_type=device_token&code=04be557473ddaf1c9e449617271235dd&client_id=23znlt3cWsHgAsbmkIsU74Id&client_secret=BjhEl2xKDFD8ailMfD9hRRrOhabIUT0D" "https://openapi.baidu.com/oauth/2.0/token"
